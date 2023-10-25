1b2a3f9825b2919579d80c45fae868d2d697efe7
Benjamin Petermann <benjamin.petermann@gmx.at>
2023-10-25 19:01:49 +0200

    Add maze class

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 from graphics import Window, Point
 from cell import Cell
+from maze import Maze
+
 
 def main():
     win = Window(800, 600)
-
-    c1 = Cell(win, Point(100, 100), Point(150, 50))
-    c1.draw()
-    c2 = Cell(win, Point(300, 300), Point(350, 250))
-    c2.draw()
-    c3 = Cell(win, Point(500, 100), Point(550, 50))
-    c3.draw()
-
-
-    c1.draw_move(c2)
-    c2.draw_move(c3)
-
+    m = Maze(200, 200, 3, 2, 50, 50, win)
     win.wait_for_close()
 
 main()
---
+++ maze.py
@@ -0,0 +1,43 @@
+from graphics import Point
+from cell import Cell
+import time
+
+class Maze:
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y= cell_size_y
+        self._cells = []
+        self.win = win
+        self._create_cells()    
+
+    def _create_cells(self):
+        self._cells = []   
+        for i in range(self.num_cols):
+            row = []
+            for j in range(self.num_rows):
+                x1 = self.x1 + i * self.cell_size_x
+                y1 = self.y1+ j * self.cell_size_y
+
+                p1 = Point(x1, y1)
+                p2 = Point(x1 + self.cell_size_x, y1 + self.cell_size_y)
+                row.append(Cell(self.win, p1, p2))
+            self._cells.append(row)
+        self._draw_cell()
+
+    def _draw_cell(self):
+        for i in range(self.num_cols):
+            for j in range(self.num_rows):
+                self._cells[i][j].draw()
+
+
+    def _animate(self):
+        self.win.redraw()
+        time.sleep(0.05)
+
+
+        
+
